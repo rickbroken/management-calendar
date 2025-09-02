041cb559d0be9feebaf4eefd2a7340a1b0aed237
Ricardo Quebrada <38635614+rickbroken@users.noreply.github.com>
2025-09-02 16:03:05 -0500

    Clean employee and dashboard pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,6 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
 import { PageContainer } from '@toolpad/core/PageContainer';
 
 export default function HomePage() {
-  
-
-  return (    
-    <PageContainer>
-      <Typography>
-        Welcome to Toolpad Core!
-      </Typography>
-    </PageContainer>
-  );
+  return <PageContainer />;
 }
